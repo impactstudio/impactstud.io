5d4c086ae810753348775ebbb730c2572bc45a26
theRingleman <sam.ringleman@gmail.com>
2016-08-10 13:14:13 -0600

    add index and show for project and add static studio page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/team', to: 'static#team'
   get '/contact', to: 'static#contact'
   get '/start-a-project', to: 'static#start_a_project'
+  get '/studio', to: 'static#studio'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
