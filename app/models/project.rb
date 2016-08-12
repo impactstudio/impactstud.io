2bc88661f91e568ff074f49ea6f99f0db62c1ace
theRingleman <sam.ringleman@gmail.com>
2016-08-12 13:31:46 -0600

    add initial paperclip code to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :clients
   has_many :testimonials
+  has_attached_file :main_image
+  validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\Z/
 end
