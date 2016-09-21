c9ac573af443974b8c3b170b0e1289d6960eb33b
theRingleman <sam.ringleman@gmail.com>
2016-09-21 07:36:31 -0600

    attempting to one line the valiadtions for image upload

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,16 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :clients
   has_many :testimonials
-  has_attached_file :main_wireframe
-  validates_attachment :main_wireframe, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-  has_attached_file :mobile_wireframe
-  validates_attachment :mobile_wireframe, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-  has_attached_file :desktop_image
-  validates_attachment :desktop_image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-  has_attached_file :logo
-  validates_attachment :logo, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-  has_attached_file :tablet
-  validates_attachment :tablet, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-  has_attached_file :phone
-  validates_attachment :phone, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  has_attached_file :main_image, :mobile_image, :desktop_image, :logo, :tablet, :phone
+  validates_attachment :main_wireframe, :mobile_wireframe, :desktop_image, :logo, :tablet, :phone,
+                       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
