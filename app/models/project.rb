115f42b242f0c7d5d5153ed6372745a6048d9134
theRingleman <sam.ringleman@gmail.com>
2016-09-21 14:49:53 -0600

    add paperclip attachment to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,7 @@ class Project < ActiveRecord::Base
   has_many :clients
   has_many :testimonials
   accepts_nested_attributes_for :clients
+  has_attached_file :main_image, :mobile_image, :desktop_image, :logo, :tablet, :phone
+  validates_attachment :main_image, :mobile_image, :desktop_image, :logo, :tablet, :phone,
+                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
