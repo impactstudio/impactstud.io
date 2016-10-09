99884d7bb14ad8eceb930579bcea58af3299700f
theRingleman <sam.ringleman@gmail.com>
2016-10-09 17:59:51 -0600

    add logo via paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009235834) do
+ActiveRecord::Schema.define(version: 20161009235929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,10 @@ ActiveRecord::Schema.define(version: 20161009235834) do
     t.string   "desktop_image_content_type"
     t.integer  "desktop_image_file_size"
     t.datetime "desktop_image_updated_at"
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
   create_table "testimonials", force: :cascade do |t|
