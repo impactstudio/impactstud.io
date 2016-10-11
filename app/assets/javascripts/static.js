71ce46a9c261940c3a8eb89644e4e0e5bf1bc2f4
JWizerd <jwodke@outlook.com>
2016-10-11 12:10:34 -0600

    jquery header content transition in and bio section media queries

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,1 +1,35 @@
+// Header content transition in from left on load
 
+function headerContentTransition(parent, delay) {
+
+  leftPositionContent(parent, delay);
+  transitionContentIn(parent, delay);
+}
+
+function leftPositionContent(parent, delay) {
+  $(parent).children().each(function(){
+    positionLeft = - window.outerWidth
+    $(this).css( {
+      "margin-left" : positionLeft.toString() + "px",
+    });
+  });
+}
+
+function transitionContentIn(parent, delay) {
+  $(parent).children().each(function(){
+    $(this).removeClass("get-off-the-page");
+    $(this).delay(delay += 150).animate({ marginLeft: 0 }, {duration: 400, easing: 'linear'});
+  });
+}
+
+function resetState(parent) {
+  $(parent).children().each(function(){
+    $(this).removeClass("get-off-the-page");
+    $(this).css({
+      "margin-left": 0
+    });
+  });
+}
+
+// I want this loaded with at the same time as the rest of the DOM
+headerContentTransition(".hero-content", 200);
