6581074374481901d0459f039d4bbc273efb9f31
JWizerd <jwodke@outlook.com>
2016-10-11 12:28:08 -0600

    fixed hero description on unflush impact logo

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,7 +1,6 @@
 // Header content transition in from left on load
 
 function headerContentTransition(parent, delay) {
-
   leftPositionContent(parent, delay);
   transitionContentIn(parent, delay);
 }
@@ -25,11 +24,12 @@ function transitionContentIn(parent, delay) {
 function resetState(parent) {
   $(parent).children().each(function(){
     $(this).removeClass("get-off-the-page");
-    $(this).css({
-      "margin-left": 0
-    });
   });
 }
 
 // I want this loaded with at the same time as the rest of the DOM
-headerContentTransition(".hero-content", 200);
+if (window.outerWidth > 531) {
+  headerContentTransition(".hero-content", 200);
+} else {
+  resetState(".hero-content");
+}
